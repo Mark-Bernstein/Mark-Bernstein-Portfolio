5d24548990f4ef83b6eec6247494e161a44786d5
Mark Bernstein <programmingbymark.com>
2022-05-31 20:44:19 -0400

    add site preferences, icon, theme, manifest, and gitignore

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import WebsiteContainer from "./components/WebsiteContainer";
 import "./App.css";
 import { Helmet } from "react-helmet";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Mark Bernstein's React Website</title>
+        <title>Mark Bernstein's Portfolio</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@300&display=swap" rel="stylesheet" />
       </Helmet>
       <WebsiteContainer />
     </div>
   );
-}
+};
 
 export default App;
